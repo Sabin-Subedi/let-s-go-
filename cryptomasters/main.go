b9a6dfae40abaa503f13ba4351e6f7e80bb31b9b
Sabin Subedi <sabinsubedi125@gmail.com>
2023-11-04 12:49:39 +0545

    cryptomasters: add the go routines for api call concurrency

--- cryptomasters/main.go
+++ cryptomasters/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "cryptomasters.com/api"
+import (
+	"cryptomasters.com/api"
+	"fmt"
+	"sync"
+)
 
 func main() {
-	rate, err := api.GetRate("BTC")
-	print(rate.Price, err)
+	currencies := []string{"BTC", "ETH", "LTC", "BCH"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1)
+		go (func(c string) {
+			getCurrencyData(c)
+			wg.Done()
+		})(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
+	if err == nil {
+		fmt.Printf("1 %s = %.2f %s\n", currency, rate.Price, "USD")
+	} else {
+		println(err.Error())
+	}
+
 }
