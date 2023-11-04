0aac507611a306efe6d22b39d69f865f1d14426f
Sabin Subedi <sabinsubedi125@gmail.com>
2023-11-04 16:13:29 +0545

    museum: started a basic server

--- museum/main.go
+++ museum/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", server)
 
 	if err != nil {
 		panic(err)
