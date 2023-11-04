0169bc7c6d6ee8b1312f1791ac328dea8e95fdff
Sabin Subedi <sabinsubedi125@gmail.com>
2023-11-04 17:22:14 +0545

    meuseum: basic server

--- museum/main.go
+++ museum/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+func hanldeTemplate(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	err = t.Execute(w, "Template")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
 
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
+	server.HandleFunc("/template", hanldeTemplate)
+	fs := http.FileServer(http.Dir("public"))
+	server.Handle("/", fs)
+
 	err := http.ListenAndServe(":8080", server)
 
 	if err != nil {
