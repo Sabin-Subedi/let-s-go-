e2b4e0fcf2b48b4203c84adb3a6c491dd7430ca2
Sabin Subedi <sabinsubedi125@gmail.com>
2023-11-04 16:06:22 +0545

    museum: started a basic server

--- museum/main.go
+++ museum/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Server is listening on port 8080")
 }
